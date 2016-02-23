a02a9742102adf3c486816dfc4c20b5c57f39edd
DmBard <dm.baryshev@gmail.com>
2016-02-23 19:08:49 +0300

    resolved merge conflicts

--- app/src/main/java/com/dmbaryshev/vkschool/view/messages/fragment/IMessagesView.java
+++ app/src/main/java/com/dmbaryshev/vkschool/view/messages/fragment/IMessagesView.java
@@ -1,11 +1,15 @@
 package com.dmbaryshev.vkschool.view.messages.fragment;
 
-import com.dmbaryshev.vkschool.model.view_model.MessageVM;
+import com.dmbaryshev.vkschool.model.dto.VkMessage;
 import com.dmbaryshev.vkschool.view.IView;
 
-public interface IMessagesView extends IView<MessageVM> {
+import java.util.List;
+
+public interface IMessagesView extends IView {
+
+    void showMessages(List<VkMessage> answer);
 
     int getIdUser();
 
-    void addMessage(MessageVM messageVM);
+    void addMessage(String messageText);
 }
